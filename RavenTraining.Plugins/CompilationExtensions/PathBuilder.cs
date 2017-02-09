424244d11aa9385bfb7c7fe1b2a594d81819fc3a
Mohammad Shahab Rafiq <Mohammad Shahab Rafiq>
2017-02-09 15:15:26 +0000

    Re-added missing code files

---
+++ RavenTraining.Plugins/CompilationExtensions/PathBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RavenTraining.Plugins.CompilationExtensions
+{
+    public static class PathBuilder
+    {
+        public static string Build(IEnumerable<string> slugs)
+        {
+            return string.Join("/", slugs).Replace("//", "/");
+        }
+    }
+}
